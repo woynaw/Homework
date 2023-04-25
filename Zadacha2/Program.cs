0742aed2c652b9930fdf12506a88f66bfdd04628
Войналович Владислав <woynaw@hotmail.com>
2023-04-25 10:37:48 +0300

    finish Zadacha2 + add Zadacha4

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) 
+{
+    Console.WriteLine($"a = {num1}; b = {num2} -> max = {num1}");
+}
+else 
+{
+    Console.WriteLine($"a = {num1}; b = {num2} -> max = {num2}");
+}
