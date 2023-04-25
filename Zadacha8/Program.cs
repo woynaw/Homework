86a574755e4b3273485366c0926b03b0a6cd5f1c
Войналович Владислав <woynaw@hotmail.com>
2023-04-25 10:51:52 +0300

    add Zadacha8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,13 @@
+﻿void Even(int arg)
+{
+    if (arg % 2 == 0) Console.Write($"{arg} ");  
+}
+Console.WriteLine("Введите N");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+Console.Write($"{n} -> ");
+while (count <= n) 
+{
+    Even(count);
+    count++;
+}
