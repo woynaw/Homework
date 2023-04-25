b02867521b5e10a090e2d30a8eb61e5041732cfa
Войналович Владислав <woynaw@hotmail.com>
2023-04-25 10:44:42 +0300

    add Zadacha6

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,9 @@
+﻿bool Even(int arg)
+{
+    if (arg % 2 == 0) return true;
+    else return false;
+}
+
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{num1} -> {Even(num1)}");
